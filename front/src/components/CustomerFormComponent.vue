862565b8684186f9e4cd9fe257d2476c9bd750ad
Juan Diego Mora Alvarado <jdmora-2021a@corhuila.edu.co>
2025-04-22 22:04:26 -0500

    Customer data component

---
+++ front/src/components/CustomerFormComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <ion-list>
+      <ion-item>
+        <ion-label position="stacked">Nombre</ion-label>
+        <ion-input v-model="name" required></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label position="stacked">Contacto</ion-label>
+        <ion-input v-model="contact" type="tel" required></ion-input>
+      </ion-item>
+    </ion-list>
+  </template>
+  
+  <script setup>
+  import { ref, watchEffect } from 'vue';
+  
+  // Definir las variables reactivas
+  const name = ref('');
+  const contact = ref('');
+  
+  // Definir los eventos que se van a emitir
+  const emit = defineEmits(['update:customer']);
+  
+  // Emitir los datos cuando cambian
+  watchEffect(() => {
+    emit('update:customer', { name: name.value, contact: contact.value });
+  });
+  </script>
+  
+  
